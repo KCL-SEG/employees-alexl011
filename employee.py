76ffab2c6180bb2475320b054b72f0a29547b133
Shih-Hung Lin <shih-hung.lin@kcl.ac.uk>
2022-10-14 23:08:24 +0100

    employees solution final

--- employee.py
+++ employee.py
@@ -2,74 +2,53 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    commission = False
-    contract = ''
-    commission_type = ''
-    salary = 0
-    def __init__(self, name, commission, contract, commission_type):
+    def __init__(self, name, pay, hours = 0, commission = 0, num_of_contracts = 0):
         self.name = name
+        self.pay = pay
+        self.hours = hours
         self.commission = commission
-        self.contract = contract
-        self.commission_type = commission_type
+        self.num_of_contracts = num_of_contracts
+
 
     def get_pay(self):
-        if(self.commission == True):
-            if(self.contract == 'salary'):
-                if(self.get_commision_type() == 'contract'):
-                    salary = 3800
-                    return salary
-                elif(self.get_commision_type() == 'bonus'):
-                    salary = 3500
-                    return salary
-            elif(self.contract == 'hourly'):
-                if(self.get_commision_type() == 'contract'):
-                    salary = 4410
-                    return salary
-                elif(self.get_commision_type() == 'bonus'):
-                    salary = 4200
-                    return salary
-        else:
-            if(self.contract == 'salary'):
-                salary = 4000
-                return salary
-            elif(self.contract == 'hourly'):
-                salary = 2500
-                return salary
-    def get_commision_type(self):
-        return self.commission_type
+        pay = self.pay
+        if self.hours > 0:
+            pay *= self.hours
+        if self.commission > 0:
+            commission = self.commission
+            if self.num_of_contracts > 0:
+                commission *= self.num_of_contracts
+            pay += commission
+        return pay
     def __str__(self):
-        if(self.commission == True):
-            if(self.contract == 'salary'):
-                if(self.get_commision_type() == 'contract'):
-                    return self.name + ' works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800. \n'
-                elif(self.get_commision_type() == 'bonus'):
-                    return self.name + ' works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500. \n'
-            elif(self.contract == 'hourly'):
-                if(self.get_commision_type() == 'contract'):
-                    return self.name + ' works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410. \n'
-                elif(self.get_commision_type() == 'bonus'):
-                    return self.name + ' works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200. \n'
+        fin_str = f'{self.name}'
+        if self.hours > 0:
+            fin_str = fin_str + f' works on a contract of {self.hours} hours at {self.pay}/hour'
         else:
-            if(self.contract == 'salary'):
-                return self.name + ' works on a monthly salary of 4000.  Their total pay is 4000. \n'
-            elif(self.contract == 'hourly'):
-                return self.name + ' works on a contract of 100 hours at 25/hour.  Their total pay is 2500. \n'
+            fin_str = fin_str + f' works on a monthly salary of {self.pay}'
+        if self.commission > 0:
+            if self.num_of_contracts > 0:
+                fin_str = fin_str + f' and receives a commission for {self.num_of_contracts} contract(s) at {self.commission}/contract'
+            else:
+                fin_str = fin_str + f' and receives a bonus commission of {self.commission}'
 
+        fin_str = fin_str + f'. Their total pay is {self.get_pay()}'
+        return fin_str
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie', False, 'salary', 'no commission')
+billie = Employee('Billie', pay = 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie', False, 'hourly', 'no commission')
+charlie = Employee('Charlie', pay = 25, hours = 100)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee', True, 'salary', 'contract')
+renee = Employee('Renee', pay = 3000, commission = 200, num_of_contracts = 4)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan', True, 'hourly', 'contract')
+jan = Employee('Jan', pay = 25, hours = 150, commission = 220, num_of_contracts = 3)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie', True, 'salary', 'bonus')
+robbie = Employee('Robbie', pay = 2000, commission = 1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel', True, 'hourly', 'bonus')
+ariel = Employee('Ariel', pay = 30, hours = 120, commission = 600)
